d2e8e002d05f667e1528b2d69ab4e3b45c868f58
Ermat <ealymbaev@gmail.com>
2023-05-24 16:34:56 +0600

    Remove unnecessary dependencies

--- Package.swift
+++ Package.swift
@@ -13,27 +13,19 @@ let package = Package(
             targets: ["BitcoinKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMinor(from: "1.2.1")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "2.0.3")),
         .package(url: "https://github.com/horizontalsystems/Hodler.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMinor(from: "1.2.1")),
         .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMinor(from: "1.2.1")),
-        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
-        .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
         .target(
             name: "BitcoinKit",
             dependencies: [
-                "BigInt",
                 .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
                 .product(name: "Hodler", package: "Hodler.Swift"),
-                .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
-                .product(name: "HsExtensions", package: "HsExtensions.Swift"),
-                .product(name: "HsToolKit", package: "HsToolKit.Swift"),
                 .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
-                .product(name: "GRDB", package: "GRDB.swift")
+                .product(name: "HsToolKit", package: "HsToolKit.Swift"),
             ]
         ),
     ]
