0a2a3eef8c98758b1de4c7c937b99a1185067dd8
Esenbek <esenbekkd@gmail.com>
2023-11-03 18:00:40 +0600

    Fix BlockchainComApi initialization bug

--- Sources/BitcoinKit/Classes/Core/Kit.swift
+++ Sources/BitcoinKit/Classes/Core/Kit.swift
@@ -66,7 +66,7 @@ public class Kit: AbstractKit {
                     let blockHashFetcher = BlockHashFetcher(hsFetcher: hsBlockHashFetcher, blockchairFetcher: blockchairBlockHashFetcher, checkpointHeight: checkpoint.block.height)
 
                     let blockchairProvider = BlockchairTransactionProvider(blockchairApi: blockchairApi, blockHashFetcher: blockHashFetcher)
-                    let blockchainComProvider = BlockchainComApi(url: "https://blockchain.info", blockHashFetcher: hsBlockHashFetcher, logger: logger)
+                    let blockchainComProvider = BlockchainComApi(url: "https://blockchain.info", blockHashFetcher: blockHashFetcher, logger: logger)
 
                     apiTransactionProvider = BiApiBlockProvider(restoreProvider: blockchainComProvider, syncProvider: blockchairProvider, apiSyncStateManager: apiSyncStateManager)
                 } else {
